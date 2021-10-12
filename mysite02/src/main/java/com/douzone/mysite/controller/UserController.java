433e83cecea829f1b170545d964c9732a78e3e63
douzone-bipa <douzone.bipa@gmail.com>
2021-10-12 13:50:21 +0900

    Action & ActionFactory

--- mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.douzone.mysite.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.douzone.web.util.MvcUtil;
+import com.douzone.web.mvc.Action;
+import com.douzone.web.mvc.ActionFactory;
 
 public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -14,13 +16,18 @@ public class UserController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		
-		String action = request.getParameter("a");
+		String actionName = request.getParameter("a");
+
+		ActionFactory af = new UserActionFactory();
+		Action action = af.getAction(actionName);
+		action.execute(request, response);
 		
-		if("joinform".equals(action)) {
-			MvcUtil.forward("/WEB-INF/views/user/joinform.jsp", request, response);
-		} else {
-			MvcUtil.redirect("/mysite02", request, response);
-		}
+//		if("joinform".equals(action)) {
+//			MvcUtil.forward("/WEB-INF/views/user/joinform.jsp", request, response);
+//		} else {
+//			MvcUtil.redirect("/mysite02", request, response);
+//		}
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
