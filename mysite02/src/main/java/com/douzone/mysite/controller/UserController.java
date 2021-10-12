3054b231a6a029e45261a5d629b614cb2c763aae
douzone-bipa <douzone.bipa@gmail.com>
2021-10-12 12:18:40 +0900

    user service #1

--- mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.douzone.mysite.controller;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,12 +8,19 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.douzone.web.util.MvcUtil;
 
-public class MainController extends HttpServlet {
+public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
-		MvcUtil.forward("/WEB-INF/views/main/index.jsp", request, response);
+		
+		String action = request.getParameter("a");
+		
+		if("joinform".equals(action)) {
+			MvcUtil.forward("/WEB-INF/views/user/joinform.jsp", request, response);
+		} else {
+			MvcUtil.redirect("/mysite02", request, response);
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
