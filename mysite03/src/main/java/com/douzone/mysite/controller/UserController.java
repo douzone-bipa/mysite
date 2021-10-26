654cb9662f5a0887e39cab3ff6b2ac1193a3eaaa
douzone-bipa <douzone.bipa@gmail.com>
2021-10-26 11:14:06 +0900

    Form Validation #2

--- mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -29,13 +30,15 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/join", method=RequestMethod.POST)
-	public String join(@Valid UserVo vo, BindingResult result) {
+	public String join(@ModelAttribute @Valid UserVo vo, BindingResult result, Model model) {
 		if(result.hasErrors()) {
 			List<ObjectError> list = result.getAllErrors();
 			for(ObjectError error : list) {
 				System.out.println(error);
 			}
 			
+			model.addAllAttributes(result.getModel());
+			// model.addAttribute("userVo", vo);
 			return "user/join";
 		}
 		
